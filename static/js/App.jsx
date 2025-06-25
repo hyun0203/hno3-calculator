d92d706accbc73154afff7b3135ad13d0b7281be
hyun0203 <papa3318@naver.com>
2025-06-25 20:18:22 +0900

    Add files via upload

---
+++ static/js/App.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>HNO3 계산기</h1>
+      <p>이곳에 계산기 기능이 표시됩니다.</p>
+    </div>
+  );
+}
+
+export default App;
