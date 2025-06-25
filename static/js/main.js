1adcc114be333c152d2ab495ff655d5f5558a861
hyun0203 <papa3318@naver.com>
2025-06-25 20:45:39 +0900

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+console.log("HNO3 계산기 메인 JS 작동 중");
